864fe4a065a5097c2a49358bb749863209c01a21
willis.hsu <shuira@gmail.com>
2017-05-13 15:00:31 +0800

    GetFile 下載檔案

--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -16,6 +16,29 @@ namespace MVC5Course.Controllers
             return View();
         }
 
+        public ActionResult SuccessRedirect()
+         {
+             //Response.Write("<script>alert('建立成功!'); location.href='/';</script>");
+             //return "<script>alert('建立成功!'); location.href='/';</script>";
+             //return Content("<script>alert('建立成功!'); location.href='/';</script>");
+             return PartialView("SuccessRedirect", "/");
+         }
+
+
+    public ActionResult PartialAbout()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("About");
+            }
+            else
+            {
+                return View("About");
+            }
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
@@ -27,5 +50,10 @@ namespace MVC5Course.Controllers
         {
             return View();
         }
-    }
+
+        public ActionResult GetFile()
+         {
+             return File(Server.MapPath("~/Content/123.jpg"), "image/jpg", "NewName.jpg");
+         }
+}
 }
